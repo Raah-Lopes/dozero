af9a2448191f98b99bcbdfb693f3a3dcbe601c6e
Raah-Lopes <raah.lopes@github.com>
2026-07-17 20:02:59 -0300

    fix: use lazy import for react-player to fix youtube support in vite

--- src/components/Audio/GlobalAudioSync.tsx
+++ src/components/Audio/GlobalAudioSync.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/lazy';
 import { state } from '../../services/yjs';
 import { useAudioStore } from '../../store/audioStore';
 
