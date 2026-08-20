b9b40a8adeeff0046ad41c4324afb8ede421977a
Raah-Lopes <rmirraine@gmail.com>
2026-08-20 16:48:34 -0300

    fix(auth): redirect to exact current room url on OAuth callback

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -55,7 +55,7 @@ export const AuthModal: React.FC = () => {
       const { error } = await supabase.auth.signInWithOAuth({
         provider,
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: window.location.href,
         },
       });
 
